7eb02318502870c5c765d88d83a6bbd164646b35
JoaoMC021 <121464880+JoaoMC021@users.noreply.github.com>
2022-12-30 17:24:39 +0000

    Create Alimentos.cpp

---
+++ Trabalhocomerro/Alimentos.cpp
@@ -0,0 +1,24 @@
+//
+// Created by joaom on 30/12/2022.
+//
+#include "Alimentos.h"
+#include "Reserva.h"
+Alimentos::Alimentos(int posX, int posY, char p) {
+
+}
+
+int Alimentos::getPosX() {
+    return 0;
+}
+
+int Alimentos::getPosY() {
+    return 0;
+}
+
+char Alimentos::gettipo() {
+    return 0;
+}
+
+Alimentos::~Alimentos() {
+
+}
